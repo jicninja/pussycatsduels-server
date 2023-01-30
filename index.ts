e93434a53b8cee6a817cfb2beba81cf3ef55099d
Ignacio Castro <ignacio@euroasiapay.com>
2023-01-30 10:46:06 -0300

    init multiplayer

--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
-import express, { type Express, type Request, type Response } from 'express';
 import { createServer, Server } from 'http';
 import { Server as SocketServer } from 'socket.io';
 import dotenv from 'dotenv';
 
+enum MultiplayerEvent {
+  play = 'play',
+  roomChange = 'roomChange',
+  roundPlayed = 'roundPlayed',
+}
+
+enum Choices {
+    ROCK = 'rock',
+    PAPER = 'paper',
+    SCISSOR = 'scissor',
+}
+
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
-const httpServer: Server = createServer(app);
+const httpServer: Server = createServer();
 
 const io = new SocketServer(httpServer, {
   cors: {
@@ -15,17 +24,40 @@ const io = new SocketServer(httpServer, {
   },
 });
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+let plays: number = 0;
 
 // eslint-ignore @typescript-eslint/no-unused-vars
 io.on('connection', (socket) => {
-  console.log('socket--->', socket);
+  const room = socket.handshake['query']['room'] || [''];
+
+  socket.join(room);
+  console.log('user joined room #' + room);
+
+  if (io.sockets.adapter.rooms.get(room as string)?.size === 2) {
+    console.log('Room #' + room + ' is ready');
+    io.to(room).emit(MultiplayerEvent.roomChange, true);
+  }
+
+  socket.on('disconnect', function () {
+    if (room) {
+      socket.leave(room as string);
+    }
+    console.log('user disconnected from ' + room);
+  });
+
+  socket.on(MultiplayerEvent.play, (choice: Choices) => {
+    socket.broadcast.to(room).emit(MultiplayerEvent.play, choice);
+    console.log('new play !', room, choice)
+
+    plays++;
+    if(plays === 2) {
+        console.log('resolve game now!!')
+        setTimeout(() => {
+            io.to(room).emit(MultiplayerEvent.roundPlayed, true);
+            plays = 0;
+        }, 1000);
+    }
+  });
 });
 
-io.listen(Number(port));
+io.listen(8080);
